4ffde201b89f9bafe5b60f5a8e292a58b60a61b4
Robson Pedroso <rbndp@hotmail.com>
2020-05-29 18:26:15 -0300

    initial project

---
+++ README.md
@@ -0,0 +1,43 @@
+# Lab - Gerar e validar JWT
+
+Projeto com os metódos de geração e validação do JWT.
+
+## Introdução
+
+Essas instruções fornecerão uma cópia do projeto em execução na sua máquina local para fins de desenvolvimento e teste.
+
+### Prerequisitos
+
+O que você precisa para baixar, rodar e disponibilizar.
+
+* Dotnet core 3.1
+* IDE de sua preferência 
+
+### Instalação
+
+Após a execução do pre requisitos, segue um passo a passo de como rodar localmente.
+
+Clonar o repositório
+
+```
+git clone git@github.com:robsonpedroso/validate-manual-jwt.git
+```
+Abra a solução com o seu IDE (no meu caso Visual Studio) e compile.
+ - Pode ser feito pelo bash, terminal ou cmd através do comando `dotnet build`
+
+## Publicação
+
+Não foi publicado
+
+## Autores
+
+* **Robson Pedroso** - *Projeto inicial* - [RobsonPedroso](https://github.com/robsonpedroso)
+
+## License
+
+Software feito apenas para fins de estudo
+
+## Ferramentas
+
+* [Dotnet](https://dotnet.microsoft.com/download)
+* [JWT](https://jwt.io/)
---
+++ ValidateManual/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+
+namespace ValidateManual
+{
+    class Program
+    {
+        static string key = "This Is My Secret Key";
+
+        static void Main(string[] args)
+        {
+            var stringToken = GenerateToken();
+
+            Console.WriteLine($"Token: { stringToken }");
+
+            ValidateToken(stringToken);
+        }
+
+        private static string GenerateToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var secToken = new JwtSecurityToken(
+                signingCredentials: credentials,
+                issuer: "Sample",
+                audience: "Sample",
+                claims: new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, "Sample")
+                },
+                expires: DateTime.Now.AddDays(5));
+
+            var handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(secToken);
+        }
+
+        private static bool ValidateToken(string authToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = GetValidationParameters();
+
+            SecurityToken validatedToken;
+            try
+            {
+                var test = tokenHandler.ValidateToken(authToken, validationParameters, out validatedToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Message: { ex.Message }");
+                Console.WriteLine($"Exception StackTrace: { ex.StackTrace }");
+                return false;
+            }
+            
+            return true;
+        }
+
+        private static TokenValidationParameters GetValidationParameters()
+        {
+            return new TokenValidationParameters()
+            {
+                ValidateLifetime = true, // Because there is no expiration in the generated token
+                ValidateAudience = false, // Because there is no audiance in the generated token
+                ValidateIssuer = false,   // Because there is no issuer in the generated token
+                ValidIssuer = "Sample",
+                ValidAudience = "Sample",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)) // The same key as the one that generate the token
+            };
+        }
+    }
+}
